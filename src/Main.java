174086ff4e52d99fcf3435c903eecfc6c88e2253
Reikon <reikon558@gmail.com>
2019-07-21 02:15:37 +0300

    Adding usage for getters

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,10 @@ public class Main {
 
     public static void main(String[] args) {
 
-	    HourlyRateEmployee john = new HourlyRateEmployee("John");
-	    FixedMonthlyRateEmployee jack = new FixedMonthlyRateEmployee("Jack");
-	    System.out.println("John's hourly salary is " + john.salary());
-	    System.out.println("Jack's fixed montly salary is " + jack.salary());
+        HourlyRateEmployee john = new HourlyRateEmployee("John");
+        FixedMonthlyRateEmployee jack = new FixedMonthlyRateEmployee("Jack");
+        System.out.println(john.getName() + "'s hourly salary is " + john.salary());
+        System.out.println(jack.getName() + "'s fixed montly salary is " + jack.salary());
 
     }
 }
